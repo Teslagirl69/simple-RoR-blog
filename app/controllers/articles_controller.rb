d4efced869a1e93c2bae626c4c36c8a296c62ca9
Teslagirl69 <teslagirl17@gmail.com>
2022-06-24 18:41:24 +0300

    implement articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,10 @@ class ArticlesController < ApplicationController
 
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
    @article = Article.new(article_params)
      if @article.valid?
