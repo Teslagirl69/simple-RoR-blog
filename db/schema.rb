827ac8768c1ade3297805c8fc57c369196ec1055
Teslagirl69 <teslagirl17@gmail.com>
2022-07-03 00:06:45 +0300

    change user_name to username

--- db/schema.rb
+++ db/schema.rb
@@ -42,10 +42,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_02_205243) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "user_name"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user_name"], name: "index_users_on_user_name", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "comments", "articles"
