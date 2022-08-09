bb8331a168270165729e2fd0ee82eb87618e155b
Teslagirl69 <teslagirl17@gmail.com>
2022-08-09 18:19:52 +0300

    Connection model article with user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,12 @@
 class Article < ApplicationRecord
-  has_many :comments
+has_many :comments, dependent: :destroy
+
+belongs_to :user, optional: true
+
+validates :title, presence: true
+validates :text, presence: true
+
+def subject
+title
+end
 end
