bc3bb4998c34a6e5aff192fc759b6c00443c0355
Teslagirl69 <teslagirl17@gmail.com>
2022-08-15 17:07:44 +0300

    update article_spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -8,4 +8,10 @@ describe Article do
   describe "assotiations" do
     it {should have_many :comments}
   end
+  describe "#subject" do
+    it "returns the article title" do
+      article = create(:article, title: 'Lorem ipsum dolor sit amet')
+      expect(article.subject).to eq 'Lorem ipsum dolor sit amet'
+    end
+  end
 end
