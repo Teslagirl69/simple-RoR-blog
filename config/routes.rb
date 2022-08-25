5fd42c2fb81d36281e569c2dc0b58dd598266900
Teslagirl69 <teslagirl17@gmail.com>
2022-08-25 14:21:36 +0300

    change routes.rb for sign_out => 'devise/sessions#destroy'

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-
-   root to: "home#index"
-
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  root to: "home#index"
   get 'home/index'
-
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
+
   resource :contacts, only:[:new, :create], path_names: {:new => ''}
   resources :articles do
     resources :comments
